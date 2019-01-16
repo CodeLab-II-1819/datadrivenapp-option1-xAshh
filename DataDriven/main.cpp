cb5a7f1db259fc2ed2f8fd8be5e274b52a67166f
Ashley Law <ashley.law17@bathspa.ac.uk>
2019-01-16 19:32:19 +0000

    Line count

--- DataDriven/main.cpp
+++ DataDriven/main.cpp
@@ -1,15 +1,54 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
-
 int main() {
 
+	ifstream file;
+	file.open("sampleTweets.csv"); //Open FIle
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	int count = 0; //variable to count lines
+	string line; //varaible to temp store line
+	if (file.good()) {//check file is ready
+		cout << "Please wait...\n" << endl; // Waiting Message
+
+		while (!file.eof()) { //loop until end of file
+			getline(file, line); //get each line of file and assign to line string
+			count++; //increment count on each loop
+		}
+		file.close(); //close file once finished
+	}
+	else {
+		cout << "File Error" << endl; //output file error if cant be read
+	}
+	
+	cout << "File has " << count << " lines." << endl; //output number of lines
 
 
 
 
 
 
+
+	system("pause");
 	return 0;
 }
